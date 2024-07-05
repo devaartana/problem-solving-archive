4cc5e93b4569f97b8a45dc0de188a8cd1a2181fc
devaartana <142993067+devaartana@users.noreply.github.com>
2024-07-05 08:06:51 +0700

    Time: 130 ms (51.34%), Space: 72.5 MB (31.79%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        
-        if(!trust.size()) return (n > 1) ? -1 : 1;
-        
+           
         vector<int> count(n+1, 0);
-        vector<bool> citizen(n+1, false);
-
         for(vector<int> edge:trust){
             count[edge[1]]++;
-            citizen[edge[0]] = true;
+            count[edge[0]]--;
         }
 
-        int max = 0, vertex;
-        for(int i=1; i <= n; i++){
-            if(count[i] > max){
-                vertex = i;
-                max = count[i];
-            }
+        for(int vertex = 1; vertex <= n; vertex++){
+            if(count[vertex] == n - 1) return vertex;
         }
 
-        return (!citizen[vertex] && count[vertex] == n - 1) ? vertex : -1;
+        return -1;
     }
 };
