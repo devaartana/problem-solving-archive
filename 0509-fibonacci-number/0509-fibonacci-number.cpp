c2d27b6fdc5c4468e589da51e9bba0f9c2a71799
devaartana <142993067+devaartana@users.noreply.github.com>
2024-07-19 05:37:47 +0800

    Time: 3 ms (67.24%), Space: 7.4 MB (5.34%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        vector<int> ans = {0, 1};
-
-        int temp;
+        if(!n) return 0;
+        
+        vector<int> ans = {0, 1, 0};
         for(int i=2; i<=n; i++){
-            temp = ans[1];
+            ans[2] = ans[1];
             ans[1] = ans[0] + ans[1];
-            ans[0] = temp;
+            ans[0] = ans[2];
         }   
 
         return ans[1];
