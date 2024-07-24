b62359834ddb3cdfbb4de6e9e1aa345c91a7a690
devaartana <142993067+devaartana@users.noreply.github.com>
2024-07-25 03:13:15 +0800

    Time: 16 ms (17.24%), Space: 16.4 MB (21.34%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -8,25 +8,25 @@ public:
         for(int i=0; i<n; i++){
             for(int j = 0; j < m; j++){
                 if(!grid[i][j]){
-                    
+
                     status =  true;
-                    queue<pair<int, int>> q;
-                    q.push({i, j});
+                    deque<pair<int, int>> q;
+                    q.push_back({i, j});
                     grid[i][j] = 1;
 
                     while(!q.empty()){
-                        auto temp = q.front();
-                        q.pop();
+                        auto [r, c] = q.front();
+                        q.pop_front();
 
-                        if(!temp.first || !temp.second || temp.first == n - 1 || temp.second == m - 1) 
+                        if(!r || !c || r == n - 1 || c == m - 1) 
                             status = false;
 
                         for(auto [mvRow, mvCol]:mv){
-                            int row = temp.first + mvRow, col = temp.second + mvCol;
+                            int row = r + mvRow, col = c + mvCol;
 
                             if(row < 0 || col < 0 || row >= n || col >= m || grid[row][col]) continue;
 
-                            q.push({row, col});
+                            q.push_back({row, col});
                             grid[row][col] = 1;
                         }
                     }
