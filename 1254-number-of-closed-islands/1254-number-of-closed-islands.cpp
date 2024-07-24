8482a647a8e6f4f9502498b79831eeca2e69b2c5
devaartana <142993067+devaartana@users.noreply.github.com>
2024-07-25 03:06:36 +0800

    Time: 20 ms (11.15%), Space: 16.6 MB (21.12%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int closedIsland(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size(), count = 0;
+
+        bool status;
+        vector<pair<int, int>> mv = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for(int i=0; i<n; i++){
+            for(int j = 0; j < m; j++){
+                if(!grid[i][j]){
+                    cout << i << " " << j << ": ";
+                    status =  true;
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    grid[i][j] = 1;
+
+                    while(!q.empty()){
+                        auto temp = q.front();
+                        q.pop();
+
+                        if(!temp.first || !temp.second || temp.first == n - 1 || temp.second == m - 1) 
+                            status = false;
+
+                        for(auto [mvRow, mvCol]:mv){
+                            int row = temp.first + mvRow, col = temp.second + mvCol;
+
+                            if(row < 0 || col < 0 || row >= n || col >= m || grid[row][col]) continue;
+
+                            q.push({row, col});
+                            grid[row][col] = 1;
+                        }
+                    }
+                    if(status) count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
