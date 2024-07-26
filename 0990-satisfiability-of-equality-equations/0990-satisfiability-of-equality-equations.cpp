308d3835aeb0e6bcb55633e6f6106f18dcf5e3ac
devaartana <142993067+devaartana@users.noreply.github.com>
2024-07-27 00:57:10 +0800

    Time: 7 ms (28.23%), Space: 15.9 MB (8.51%) - LeetHub

---
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int find_parent(vector<int> &parent, int v){
+        if (v == parent[v])
+            return v;
+
+        return parent[v] = find_parent(parent, parent[v]);
+    }
+
+    void union_set(vector<int> &parent, int vertex1, int vertex2){
+        int parent1 = find_parent(parent, vertex1);
+        int parent2 = find_parent(parent, vertex2);
+
+        if (parent1 != parent2)
+            parent[parent2] = parent1;
+    }
+    
+    bool equationsPossible(vector<string>& equations) {
+        vector<int> parent(27, 0);
+        vector<vector<bool>> relationships(27, vector<bool>(27, false));
+
+        for(int i=0; i<27; i++){
+            parent[i] = i;
+        }
+
+        int x, y;
+        for(auto eq:equations){
+            if(eq[1] == '='){
+                x = eq[0] - 'a';
+                y = eq[3] - 'a';
+
+                union_set(parent, x, y);
+            }
+        }
+
+        for(int i=0; i<27; i++){
+            parent[i] = find_parent(parent, i);
+        }
+
+        for(auto eq:equations){
+            if(eq[1] == '!'){
+                x = eq[0] - 'a';
+                y = eq[3] - 'a';
+
+                if(parent[x] == parent[y]) return false;
+            }
+        }
+
+        return true;
+    }
+};
