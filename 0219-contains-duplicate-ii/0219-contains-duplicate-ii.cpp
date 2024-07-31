d2f40a193afb84f41fd243e08a1ced691c75114f
devaartana <142993067+devaartana@users.noreply.github.com>
2024-07-31 16:19:04 +0800

    Time: 125 ms (44.32%), Space: 81 MB (49.46%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int, int> discovered;
 
         for(int i=0; i<nums.size(); i++){
-            if(discovered[nums[i]] && abs(i + 1 - discovered[nums[i]]) <= k){
+            if(discovered[nums[i]] && (i + 1 - discovered[nums[i]] <= k)){
                 return true;
             }
 
