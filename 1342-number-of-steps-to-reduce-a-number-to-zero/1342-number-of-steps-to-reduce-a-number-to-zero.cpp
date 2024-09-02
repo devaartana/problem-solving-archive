1fe1188f4d73f384611ad41fda8c0fcd36643c2d
devaartana <142993067+devaartana@users.noreply.github.com>
2024-09-03 00:53:29 +0700

    Time: 0 ms (100%), Space: 7.4 MB (71.89%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int step = 0;
+
+        while(num){
+            if(num&1) num--;
+            else num/=2; 
+
+            step++;
+        }
+
+        return step;
+    }
+};
