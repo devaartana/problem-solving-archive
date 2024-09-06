36b73ff6e5b1fda8fe3bc9538905da47696c741c
devaartana <142993067+devaartana@users.noreply.github.com>
2024-09-07 06:56:52 +0700

    Time: 0 ms (100%), Space: 7.7 MB (11.85%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0; i < s.length(); i++){
+            if(s[i] >= 'A' && s[i] <='Z') s[i] += 32;
+        }
+
+        return s;
+    }
+};
