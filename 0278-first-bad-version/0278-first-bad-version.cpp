744af4f490d0b9ca8d20b4fff4290ced3cc5b6d1
devaartana <142993067+devaartana@users.noreply.github.com>
2024-10-05 17:34:58 +0700

    Time: 0 ms (100%), Space: 7.5 MB (85.32%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -6,11 +6,9 @@ public:
     int firstBadVersion(int n) {
         int lo = 1, hi = n;
 
-        while(lo < hi){
+        while(lo <= hi){
             int mid = (hi - lo)/2 + lo;
-            bool statusMid = isBadVersion(mid), beforeMid = isBadVersion(mid - 1);
-            if(statusMid && !beforeMid) return mid;
-            else if(statusMid) hi = mid - 1;
+            if(isBadVersion(mid)) hi = mid - 1;
             else lo = mid + 1;
         }
 
