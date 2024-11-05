d40a5a9825b4159d0fa309190ed8b1ee527f8332
devaartana <142993067+devaartana@users.noreply.github.com>
2024-11-05 08:43:25 +0700

    Time: 0 ms (100%), Space: 12.2 MB (67.52%) - LeetHub

---
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minChanges(string s) {
+        int ans = 0;
+        for(int i=0; i < s.length(); i+=2) {
+            if(s[i] != s[i + 1]) ans++;
+        }
+
+        return ans;
+    }
+};
