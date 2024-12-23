8e779fde3182839ca205d40ab6576d358d07ed8f
devaartana <142993067+devaartana@users.noreply.github.com>
2024-12-23 14:29:13 +0700

    Time: 43 ms (17.52%), Space: 23.5 MB (93.7%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, int> ump;
+        vector<vector<string>> ans;
+
+        int i = 0;
+        for (string str : strs) {
+            sort(str.begin(), str.end());
+            auto result = ump.find(str);
+            if (result != ump.end()) {
+                ans[result->second].push_back(strs[i]);
+            } else {
+                ump[str] = ans.size();
+                ans.push_back({strs[i]});
+            }   
+            i++;
+        }
+
+        for(auto data : ump) {
+            cout << data.first << endl; 
+        }
+
+        return ans;
+    }
+};
